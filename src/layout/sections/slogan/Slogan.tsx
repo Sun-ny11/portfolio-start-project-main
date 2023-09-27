ff4650a2bb93ace5ea2c12eeb8b11f2919d6f0be
Sun-ny11 <spetrow79@gmail.com>
2023-09-27 14:16:00 +0300

    style for slogan and footer

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -3,13 +3,17 @@ import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
 import { Container } from "../../../components/Container";
+import { FlexWrapper } from "../../../components/FlexWrapper";
 
 export const Slogan =() => {
    return (
       <StyledSlogan>
          <Container>
-            <SectionTitle>I Am Available For Freelance</SectionTitle>
+            <FlexWrapper direction="column" align="center">
+               <SectionTitle>I Am Available For Freelance</SectionTitle>
                <Button>Hire me</Button>
+            </FlexWrapper>
+            
          </Container>
          
       </StyledSlogan>
